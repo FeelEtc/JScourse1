a44b131a3c959c9640587d435a8be0279bf679c5
FeelEtc <filippoff.a.n@gmail.com>
2020-04-13 19:26:43 +0300

    Добавил дз по курсу "Интенсив в JS", Udemy

---
+++ script.js
@@ -0,0 +1,30 @@
+let c = "классные сиськи"; 
+alert (c);
+
+// while (true) {
+//    table = 5;
+// var table = 17; 
+// alert ("я ебал гусей");
+// alert ("нихуя");
+let n = 5;
+n = 10;
+let m = 7;
+m = 20;
+let p = n + m;
+let s = n * m;
+// alert(s);
+let imp = 4 * p;
+var hui = "long23";
+// alert(hui);
+// alert(imp);
+// alert(p);
+// alert("pizdec" + 3 * p );
+// alert(n);
+// alert(m);
+// alert(p);
+// alert(n + m + p);
+// alert("pizdec" * n);
+// alert(m / n);
+// }
+alert(24 + n);
+
