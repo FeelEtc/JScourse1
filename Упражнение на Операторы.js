de40fc184976f9678de76024524390f6ad3bfb3e
FeelEtc <filippoff.a.n@gmail.com>
2020-04-17 18:24:39 +0300

    Решаю задачку от Шведа

--- Упражнение на Операторы.js
+++ Упражнение на Операторы.js
@@ -1,7 +1,31 @@
-alert( 5 > 4); //true
+//упражнение после раздела 2.8 "Операторы сравнения" (учебник learn.javascript.ru)
+
+/* alert( 5 > 4); //true
 alert( "ананас" > "яблоко" ); //false
 alert( "2" > "12" ); //true
 alert( undefined == null ); //true
 alert( undefined === null ); //false
 alert ( null == "\n0\n" ); //false
-alert ( null === +"\n0\n" ); //false
+alert ( null === +"\n0\n" ); //false */
+
+//Задача от Шведа. Написать код, который проверит: есть ли пользователю 18 лет и девственник ли он (решить, используя if...else, prompt, alert)
+
+
+let UserAge = confirm("Тебе есть 18?"); //вначале решил сделать при помощи confirm, т.к. такое решение показалось легче
+if (UserAge == false) {
+    alert("не дрочи! Учи JS!");
+} else {
+   let SexStatus = confirm("Был ли у тебя секс?");
+   if (SexStatus == true) {
+       alert("ты мачо!");
+   }    else {
+       alert("ты девственник!");
+   }
+} //код работает. Программа выполняется
+
+/* let UserAge = prompt("Тебе есть 18?", " ");
+if (UserAge == "нет") {
+    alert("не дрочи! Учи JS!");
+// }   else {
+
+// } */
